c8f2214d79f5375d4c8b71ca2063af8802537606
lakshmivenkateswara2356 <lakshmivenkateswarapunnam@gmail.com>
2026-01-29 18:19:41 +0530

    checkingthechanges

--- onegramgoldfancy-main/backend/routes/product.routes.js
+++ onegramgoldfancy-main/backend/routes/product.routes.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middleware/upload"); // multer cloudinary
+const upload = require("../middleware/upload"); // multer Cloudinary
 const productController = require("../controllers/product.controller");
 
-// ADD PRODUCT (single or multiple images)
-router.post(
-  "/add",
-  upload.array("images", 5),
-  productController.addProduct
-);
+// ADD PRODUCT (multi-image)
+router.post("/add", upload.array("images", 5), productController.addProduct);
 
 // GET ALL PRODUCTS
 router.get("/", productController.getProducts);
 
+// GET SINGLE PRODUCT
+router.get("/:id", productController.getSingleProduct);
+
 // UPDATE PRODUCT
-router.put(
-  "/:id",
-  upload.array("images", 5),
-  productController.editProduct
-);
+router.put("/:id", upload.array("images", 5), productController.editProduct);
 
 // DELETE PRODUCT
 router.delete("/:id", productController.removeProduct);
