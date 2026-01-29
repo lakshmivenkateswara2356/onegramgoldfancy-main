e512d28a8ffd6589a4229954192ff7714ccba92e
lakshmivenkateswara2356 <lakshmivenkateswarapunnam@gmail.com>
2026-01-29 16:49:09 +0530

    changesmaded

--- onegramgoldfancy-main/backend/routes/product.routes.js
+++ onegramgoldfancy-main/backend/routes/product.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const upload = require("../middlewares/uploads"); // multer cloudinary
+const upload = require("../middlewares/upload"); // multer cloudinary
 const productController = require("../controllers/product.controller");
 
 // ADD PRODUCT (single or multiple images)
