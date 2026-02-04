cefea78884dde84e243d766a6a3a976c27a39543
lakshmivenkateswara2356 <lakshmivenkateswarapunnam@gmail.com>
2026-02-04 10:08:11 +0530

    rmvdusfct

--- onegramgoldfancy-main/frontend/src/Pages/Cart.jsx
+++ onegramgoldfancy-main/frontend/src/Pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext,  useState } from "react";
 import { AppContext } from "../context/AppContext";
 import Navbar from "../Components/Navbar";
 import axios from "axios";
